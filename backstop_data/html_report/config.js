a69ac35d2ac1609742021cd78e8eb26f544cfdf0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-11 22:46:59 +0200

    Sample output from border-radius detection tests

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,44 +4,48 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Casper_Theme_0_document_0_phone.png",
-        "test": "../bitmaps_test/20170911-223210/backstop_default_Casper_Theme_0_document_0_phone.png",
+        "test": "../bitmaps_test/20170911-224447/backstop_default_Casper_Theme_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_Casper_Theme_0_document_0_phone.png",
         "label": "Casper Theme",
         "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
+        "misMatchThreshold": 0.01,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00",
+          "misMatchPercentage": "0.05",
+          "analysisTime": 78,
           "getDiffImage": null
-        }
+        },
+        "diffImage": "../bitmaps_test/20170911-224447/failed_diff_backstop_default_Casper_Theme_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Casper_Theme_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20170911-223210/backstop_default_Casper_Theme_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20170911-224447/backstop_default_Casper_Theme_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_Casper_Theme_0_document_1_tablet.png",
         "label": "Casper Theme",
         "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
+        "misMatchThreshold": 0.01,
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00",
+          "misMatchPercentage": "0.03",
+          "analysisTime": 153,
           "getDiffImage": null
-        }
+        },
+        "diffImage": "../bitmaps_test/20170911-224447/failed_diff_backstop_default_Casper_Theme_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ]
 });
