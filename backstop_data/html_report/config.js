266bd971daf6222abb71d6b08ec06d0609c932fe
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-11 22:34:35 +0200

    Backstop report

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,47 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Casper_Theme_0_document_0_phone.png",
+        "test": "../bitmaps_test/20170911-223210/backstop_default_Casper_Theme_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Casper_Theme_0_document_0_phone.png",
+        "label": "Casper Theme",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Casper_Theme_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20170911-223210/backstop_default_Casper_Theme_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Casper_Theme_0_document_1_tablet.png",
+        "label": "Casper Theme",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
